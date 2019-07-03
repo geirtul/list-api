fec8a345cc8c41126b52f3319dd09ece8101e7ca
Geir Tore Ulvik <ulvik@luring.uio.no>
2019-07-04 01:38:39 +0200

    Added simple functions for displaying endpoints as json, some comments, and link to guide I'm following.

--- main.go
+++ main.go
@@ -1,43 +1,81 @@
 package main
 
+/*
+Following this guide for RESTful API in go:
+https://medium.com/@johnteckert/building-a-restful-api-with-go-part-1-9e234774b14d
+*/
+
 import (
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"strconv"
 )
 
 // ========== Structs ==========
 
-// Checklist implements ...
-type Checklist struct {
-	ID    string `json:"id,omitempty"`
-	Items string `json:"items,omitempty"`
+// List implements ...
+type List struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
+
+var lists []List
 
 // Functions
 
-// GetChecklists ...
-func GetChecklists(w http.ResponseWriter, r *http.Request) {
+// GetLists ... indexes lists
+func GetLists(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(lists)
 }
 
-// GetChecklist ...
-func GetChecklist(w http.ResponseWriter, r *http.Request) {
+// GetList ... show specific checklist
+func GetList(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range lists {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
 }
 
-// CreateChecklist ...
-func CreateChecklist(w http.ResponseWriter, r *http.Request) {
+// CreateList ... create new checklist
+func CreateList(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newList List
+	json.NewDecoder(r.Body).Decode(&newList)
+	newList.ID = strconv.Itoa(len(lists) + 1)
+	lists = append(lists, newList)
+	json.NewEncoder(w).Encode(newList)
 }
 
-// DeleteChecklist ...
-func DeleteChecklist(w http.ResponseWriter, r *http.Request) {
+// UpdateList ... update a checklist
+func UpdateList(w http.ResponseWriter, r *http.Request) {
+}
+
+// DeleteList ... delete checklist
+func DeleteList(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	// Sample data
+	lists = append(lists,
+		List{ID: "1", Name: "Katter"},
+		List{ID: "2", Name: "Planter"})
+
+	// Initialize router
 	router := mux.NewRouter()
-	router.HandleFunc("/checklists", GetChecklists).Methods("GET")
-	router.HandleFunc("/checklists/{id}", GetChecklist).Methods("GET")
-	router.HandleFunc("/checklists/{id}", CreateChecklist).Methods("POST")
-	router.HandleFunc("/checklists/{id}", DeleteChecklist).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", router))
+
+	// Endpoints
+	router.HandleFunc("/lists", GetLists).Methods("GET")
+	router.HandleFunc("/lists/{id}", GetList).Methods("GET")
+	router.HandleFunc("/lists/{id}", CreateList).Methods("POST")
+	router.HandleFunc("/lists/{id}", DeleteList).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
